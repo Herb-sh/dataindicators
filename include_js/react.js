2b83eac69cd5000b9b580f8ebc164b0b6b5b78cc
crs <herbi.s@scopicsoftware.com>
2017-03-05 02:13:17 +0100

    Debugging

--- include_js/react.js
+++ include_js/react.js
@@ -8,8 +8,6 @@ var IndicatorRow = React.createClass({
         var indicators = [];
 
         for (var key in this.props.indicatorsOfGroup) {
-            /* @NOTE To change,
-             * Pump price for gasoline selected */
             if (key === app.CONFIG.defaultIndicator) {
                 indicators.push(React.createElement(
                     "option",
@@ -44,13 +42,8 @@ var IndicatorGroup = React.createClass({
 
         var indicatorsGroup = [];
 
-        // indicators.push(<option value="" disabled selected>Choose an indicator</option>);
         for (var key in this.props.indicatorsGroups) {
-
-            //          console.log(key);
-
             var indicatorsOfGroup = this.props.indicatorsGroups[key].indicators;
-
             indicatorsGroup.push(React.createElement(IndicatorRow, { key: key, k: key, indicatorsOfGroup: indicatorsOfGroup }));
         }
 
