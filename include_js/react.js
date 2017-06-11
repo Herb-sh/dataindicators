633a6d8a42371f49517ddbb41ab76bdfceee695e
crs <herbi.s@scopicsoftware.com>
2017-06-12 00:15:49 +0200

    Small changes

--- include_js/react.js
+++ include_js/react.js
@@ -1,9 +1,10 @@
 "use strict";
 
+/** Indicators Group */
 var IndicatorRow = React.createClass({
     displayName: "IndicatorRow",
 
-    render: function render() {
+    render: function render () {
 
         var indicators = [];
 
@@ -11,14 +12,14 @@ var IndicatorRow = React.createClass({
             if (key === app.CONFIG.defaultIndicator) {
                 indicators.push(React.createElement(
                     "option",
-                    { value: key, selected: "selected" },
+                    {value: key, selected: "selected"},
                     " ",
                     this.props.indicatorsOfGroup[key]
                 ));
             } else {
                 indicators.push(React.createElement(
                     "option",
-                    { value: key },
+                    {value: key},
                     " ",
                     this.props.indicatorsOfGroup[key]
                 ));
@@ -26,7 +27,7 @@ var IndicatorRow = React.createClass({
         }
         return React.createElement(
             "optgroup",
-            { label: this.props.k },
+            {label: this.props.k},
             indicators
         );
     }
@@ -35,30 +36,34 @@ var IndicatorRow = React.createClass({
 var IndicatorGroup = React.createClass({
     displayName: "IndicatorGroup",
 
-    change: function change(e) {
+    change: function change (e) {
         app.getByIndicator(e.target.value);
     },
-    render: function render() {
+    render: function render () {
 
         var indicatorsGroup = [];
 
         for (var key in this.props.indicatorsGroups) {
             var indicatorsOfGroup = this.props.indicatorsGroups[key].indicators;
-            indicatorsGroup.push(React.createElement(IndicatorRow, { key: key, k: key, indicatorsOfGroup: indicatorsOfGroup }));
+            indicatorsGroup.push(React.createElement(IndicatorRow, {
+                key: key,
+                k: key,
+                indicatorsOfGroup: indicatorsOfGroup
+            }));
         }
 
         return React.createElement(
             "div",
-            { className: "indicators" },
+            {className: "indicators"},
             React.createElement(
                 "form",
                 null,
                 React.createElement(
                     "fieldset",
-                    { className: "form-group" },
+                    {className: "form-group"},
                     React.createElement(
                         "select",
-                        { id: "indicatorSelect", className: "c-select", onChange: this.change },
+                        {id: "indicatorSelect", className: "c-select", onChange: this.change},
                         indicatorsGroup
                     )
                 )
@@ -67,6 +72,55 @@ var IndicatorGroup = React.createClass({
     }
 });
 
+/** Year Block */
+var YearRow = React.createClass({
+    displayName: "YearRow",
+
+    render: function render () {
+
+        var indicators = [];
+
+        for (var key in this.props.indicatorsOfGroup) {
+            if (key === app.CONFIG.defaultIndicator) {
+                indicators.push(React.createElement(
+                    "option",
+                    {value: key, selected: "selected"},
+                    " ",
+                    this.props.indicatorsOfGroup[key]
+                ));
+            } else {
+                indicators.push(React.createElement(
+                    "option",
+                    {value: key},
+                    " ",
+                    this.props.indicatorsOfGroup[key]
+                ));
+            }
+        }
+        return React.createElement(
+            "optgroup",
+            {label: this.props.k},
+            indicators
+        );
+    }
+});
+
+var YearGroup = React.createClass({
+    displayName: "YearGroup",
+
+    change: function (e) {
+        app.getByIndicator(e.target.value);
+    },
+
+    render: function () {
+
+    }
+});
+
 ReactDOM.render(React.createElement(IndicatorGroup,
-    { indicatorsGroups: indicatorsSelected }),
+    {
+        indicatorsGroups: appData.getIndicatorsSelected()
+        // ,
+        // yearsGroups: appData.getTndicatorYears()
+    }),
     document.getElementById('components'));
